e4fb3b682cb631234e872c41e28e453f677366b1
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-15 18:44:07 -0300

    :gem: last activities page

---
+++ webapp/resources/js/Components/AddNewUser.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modal fade" id="addNewUser" tabindex="-1" role="dialog" aria-labelledby="addNewUserLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addNewUserLabel">Adicionar novo cliente</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="name">Nome</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">E-mail</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="name">Foto</label>
+                            <input type="file" class="form-control">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">
+                        Cancelar
+                    </button>
+                    <button type="button" class="btn btn-info btn-sm">
+                        <i class="fa-regular fa-square-plus"></i> Cadastrar cliente
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- webapp/resources/js/Pages/Dashboard.vue
+++ webapp/resources/js/Pages/Dashboard.vue
@@ -1,30 +1,238 @@
 <script setup>
+import AddNewUser from '@/Components/AddNewUser.vue';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                <div class="text-right mb-3">
+                    <button class="btn btn-sm btn-info" data-toggle="modal" data-target="#addNewUser">
+                        <i class="fa-regular fa-square-plus"></i> Cadastrar cliente
+                    </button>
+                </div>
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        You're logged in!
+                        <h5 class="mb-4">Últimas atividades registradas</h5>
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>E-mail</th>
+                                    <th>Status</th>
+                                    <th>Horário</th>
+                                    <th>Dentro do horário</th>
+                                    <th>Ver foto</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>José da Silva</td>
+                                    <td>jose@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:00:00</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Dentro do horário
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            Não encontrado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:03:00</td>
+                                    <td>--</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Adriano da Silva</td>
+                                    <td>adriano@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:06:00</td>
+                                    <td>
+                                        <span class="badge badge-warning">
+                                            6 minuto(s) atrasado
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Karen da Silva</td>
+                                    <td>karen@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:11:00</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            11 minuto(s) atrasado
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            Não encontrado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:03:00</td>
+                                    <td>--</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>José da Silva</td>
+                                    <td>jose@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:00:00</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Dentro do horário
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            Não encontrado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:03:00</td>
+                                    <td>--</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Adriano da Silva</td>
+                                    <td>adriano@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:06:00</td>
+                                    <td>
+                                        <span class="badge badge-warning">
+                                            6 minuto(s) atrasado
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Karen da Silva</td>
+                                    <td>karen@email.com</td>
+                                    <td>
+                                        <span class="badge badge-success">
+                                            Confirmado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:11:00</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            11 minuto(s) atrasado
+                                        </span>
+                                    </td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>--</td>
+                                    <td>--</td>
+                                    <td>
+                                        <span class="badge badge-danger">
+                                            Não encontrado
+                                        </span>
+                                    </td>
+                                    <td>01/02/2024 09:03:00</td>
+                                    <td>--</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-outline-info">
+                                            <i class="fa-solid fa-eye"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
+
+
+            <AddNewUser />
+
         </div>
+
     </AuthenticatedLayout>
 </template>
